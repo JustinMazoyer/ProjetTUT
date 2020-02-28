d64fc3ea7010e4ed57d994aa6d9fc354061183bc
Justin <Justin@DESKTOP-ROOVLK1>
2020-02-28 18:10:19 +0100

    Creation du projet

--- src/main/java/controller/SoumettreDispositif.java
+++ src/main/java/controller/SoumettreDispositif.java
@@ -9,18 +9,24 @@ import Classe.dao.DispositifFacade;
 import Classe.entity.Dispositif;
 import form.DispositifForm;
 import javax.inject.Inject;
+import javax.mvc.Controller;
 import javax.mvc.Models;
+import javax.mvc.View;
 import javax.mvc.binding.BindingResult;
 import javax.validation.Valid;
 import javax.validation.executable.ExecutableType;
 import javax.validation.executable.ValidateOnExecution;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 
 /**
  *
  * @author Justin
  */
+@Controller
+@Path("SoumettreDispositif")
+@View("SoumettreDispositif.jsp")
 public class SoumettreDispositif {
     @Inject
     DispositifFacade facadeP;
